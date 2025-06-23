a363060002eab9ca4c5f7d7c4fedf2a9ad99e46e
Abdur Rauf <raufabdur886@gmail.com>
2025-06-24 03:19:56 +0500

    `Update server routes with modified category names`

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,13 +5,16 @@ export const serverRoutes: ServerRoute[] = [
     path: 'quiz/:category',
     renderMode: RenderMode.Prerender,
     getPrerenderParams: async () => {
-      const categories = ['computer science', 'general knowledge', 'biology'];
+      const categories = [
+        'computer-science',
+        'general-knowledge',
+        'biology'
+      ];
       return categories.map(category => ({ category }));
     }
   },
   {
     path: '**',
-renderMode: RenderMode.Server
-
+    renderMode: RenderMode.Server
   }
 ];
