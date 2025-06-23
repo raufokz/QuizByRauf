f6559ad4db04c585a5aeaac3fd2429134542169a
Abdur Rauf <raufabdur886@gmail.com>
2025-06-24 03:16:03 +0500

    `Added prerender route for quiz by category and changed default route render mode to Server`

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'quiz/:category',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const categories = ['computer science', 'general knowledge', 'biology'];
+      return categories.map(category => ({ category }));
+    }
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+renderMode: RenderMode.Server
+
   }
 ];
